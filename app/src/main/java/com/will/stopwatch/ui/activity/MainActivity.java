2093c2391a0ddcfc15c5675aa4e0cfa7d0c104e9
wilkison martins <wilkisonmartinsdasilva@gmail.com>
2021-02-16 10:13:19 -0400

    primeiro commit

---
+++ app/src/main/java/com/will/stopwatch/ui/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.will.stopwatch.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.will.stopwatch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
